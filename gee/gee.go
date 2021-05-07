1dc4dc77c7d3d5d9203eeb0644ab8a411528643e
florianzhou <florianzhou@aaxischina.com>
2021-05-07 16:44:43 +0800

    第一天---http 自定义一个map 路由

--- gee/gee.go
+++ gee/gee.go
@@ -1,7 +1,17 @@
 package gee
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+/**
+首先定义了类型HandlerFunc，这是提供给框架用户的，用来定义路由映射的处理方法。我们在Engine中，
+添加了一张路由映射表router，key 由请求方法和静态路由地址构成，例如GET-/、GET-/hello、POST-/hello，
+这样针对相同的路由，如果请求方法不同,可以映射不同的处理方法(Handler)，value 是用户映射的处理方法。
+当用户调用(*Engine).GET()方法时，会将路由和处理方法注册到映射表 router 中，(*Engine).Run()方法，是 ListenAndServe 的包装。
+Engine实现的 ServeHTTP 方法的作用就是，解析请求的路径，查找路由映射表，如果查到，就执行注册的处理方法。如果查不到，就返回 404 NOT FOUND 。
+*/
 // 首先我们定义一个给大家科普一下:
 //海王：吊着你的今晚吃啥
 //炮王：约炮和吃饭一样的今晚吃啥
@@ -69,8 +79,11 @@ func (e *Engine) Run(address string) (err error) {
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	// 获取到-key  然后取出 实现HandlerFunc 的方法
 	key := req.Method + "-" + req.URL.Path
-	handler := e.router[key]
-	handler(w, req)
+	if handler, ok := e.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc) {
